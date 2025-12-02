e6ddb818ae3125961c9625b3da6c78e784d8dcd3
SoftDevCLF <cintya.lara.f@gmail.com>
2025-12-02 16:43:58 -0700

    create nav bar with home button

---
+++ app/components/nav-bar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full  bg-[#A6192E] text-white px-6 py-3">
+      <ul className="flex gap-6">
+        <li className="cursor-pointer hover:opacity-80">Home</li>
+      </ul>
+    </nav>
+  );
+}
