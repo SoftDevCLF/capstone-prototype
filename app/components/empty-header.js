92aef2b424bc36572bfb2ebd0c26cd576f42471a
Temi Bankole <temibankole5@gmail.com>
2025-12-03 14:28:21 -0700

    adding use path name hook to not show the logout button

--- app/components/empty-header.js
+++ app/components/empty-header.js
@@ -1,15 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import LogoutButton  from "./LogoutButton";
+import { usePathname } from "next/navigation";
 
-export default function EmptyHeader({ onLogout }) {
+export default function EmptyHeader() {
+  const pathname = usePathname(); // get current URL path
+  const isLoginPage = pathname === "/login"; // adjust if your login path is different
+
   return (
     <div className="container">
       <header className="w-full bg-white py-4 px-6 shadow sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32 flex items-center justify-between">
         {/* Left: Logo */}
         <Image src="/sait-logo.png" alt="Logo" height={36} width={131} />
 
-        {/* Right: Logout Button */}
-        <LogoutButton/>
+        {/* Only show logout button if NOT on the login page */}
+        {!isLoginPage && <LogoutButton />}
       
       </header>
     </div>
