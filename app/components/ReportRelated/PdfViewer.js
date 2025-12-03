ae5f98ee112d1cbcfbf4af88d5d3ea1a1aa7aa6e
Temi Bankole <temibankole5@gmail.com>
2025-12-02 19:21:25 -0700

    create pdf viewer aspects

--- app/components/ReportRelated/PdfViewer.js
+++ app/components/ReportRelated/PdfViewer.js
@@ -1,1 +1,15 @@
 //Functionality to display a PDF preview will be created here
+
+import { Document, Page } from 'react-pdf';
+
+export default function PDFViewer({ pdfData }) {
+  return (
+    <div className="border rounded-md w-[600px]">
+      {pdfData && (
+        <Document file={pdfData}>
+            <Page pageNumber={1}/>
+        </Document>
+      )}
+    </div>
+  )
+}
