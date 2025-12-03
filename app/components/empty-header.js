43d7f8f90c1ec317da82c908d78edc6464c830b7
SoftDevCLF <cintya.lara.f@gmail.com>
2025-12-02 20:13:08 -0700

    modify container

--- app/components/empty-header.js
+++ app/components/empty-header.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
 export default function EmptyHeader() {
   return (
-    <header className="w-full bg-white py-4 px-6 shadow">
-      <Image src="/sait-logo.png" alt="Logo" height={36} width={131} />
-    </header>
+    <div className="container">
+      <header className="w-full bg-white py-4 px-6 shadow sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32">
+        <Image src="/sait-logo.png" alt="Logo" height={36} width={131} />
+      </header>
+    </div>
   );
 }
