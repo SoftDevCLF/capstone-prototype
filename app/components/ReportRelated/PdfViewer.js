b8d9792d2af00200a265ca54c1f7efb3f4006a93
Temi Bankole <temibankole5@gmail.com>
2025-12-02 23:01:33 -0700

    PDF viewer

--- app/components/ReportRelated/PdfViewer.js
+++ app/components/ReportRelated/PdfViewer.js
@@ -1,14 +1,30 @@
 //Functionality to display a PDF preview will be created here
-import { Document, Page } from 'react-pdf';
+//ADD PRINT AND DOWNLOAD BUTTONS
+import { Document, Page, pdfjs } from 'react-pdf';
+
+//To Render PDF
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function PDFViewer({ pdfData }) {
   return (
-    <div className="border rounded-md w-[600px]">
+  <div className="w-full flex">
+  <div className="md:w-[800px] min-h-[200px] p-6 bg-[#EDEDF0] border border-gray-200">
+      {!pdfData && (
+        <div className="h-[700px] overflow-y-auto p-3 text-center">
+          No report generated yet.
+        </div>
+      )}
+
       {pdfData && (
-        <Document file={pdfData}>
-            <Page pageNumber={1}/>
+        <Document
+          file={pdfData}
+          loading={<div className="text-center text-gray-500">Loading PDF…</div>}
+          error={<div className="text-center text-red-600">Failed to load PDF.</div>}
+        >
+          <Page pageNumber={1} />
         </Document>
       )}
     </div>
-  )
+    </div>
+  );
 }
