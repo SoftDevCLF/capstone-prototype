581bebef7a8ccbd16c4fc8ed8716d6619b19722e
Temi Bankole <temibankole5@gmail.com>
2025-12-02 19:21:40 -0700

    create report buttons

--- app/components/ReportRelated/ReportButtons.js
+++ app/components/ReportRelated/ReportButtons.js
@@ -1,1 +1,32 @@
+"use client";
 //Buttons such as Create Report, Download, Print, Create Another Report(or clear)
+
+export default function ReportButtons({ onPrint, onDownload, onClear }) {
+  return (
+    <div className="flex gap-4 mt-4">
+      {/*Print Button*/}
+      <button 
+        className="bg-blue-600 text-white px-4 py-2 rounded"
+        onClick={onPrint}
+      >
+        Print
+      </button>
+
+      {/*Download Button*/}
+      <button 
+        className="bg-green-600 text-white px-4 py-2 rounded"
+        onClick={onDownload}
+      >
+        Download
+      </button>
+
+      {/*Create New Report Button*/}
+      <button 
+        className="bg-gray-500 text-white px-4 py-2 rounded"
+        onClick={onClear}
+      >
+        Create New Report
+      </button>
+    </div>
+  );
+}
