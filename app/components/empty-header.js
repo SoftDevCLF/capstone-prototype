b7b4c39960c3b79b29374372b2bc6f1e78d54834
Temi Bankole <temibankole5@gmail.com>
2025-12-02 19:32:55 -0700

    add logout button

--- app/components/empty-header.js
+++ app/components/empty-header.js
@@ -1,8 +1,13 @@
 import Image from "next/image";
 export default function EmptyHeader() {
   return (
-    <header className="w-full bg-white py-4 px-6 shadow">
+    <header className="w-full bg-white py-4 px-6 shadow flex justify-between items-center">
       <Image src="/sait-logo.png" alt="Logo" height={36} width={131} />
+      
+      {/* Right Logout Button */}
+      <button className="text-white bg-[#005EB8] border-white border px-4 py-1 rounded hover:bg-[#004D96] transition">
+        Logout
+      </button>
     </header>
   );
 }
