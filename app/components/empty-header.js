c430b25d2faf6a02397d22795b71e9b316c334d9
SoftDevCLF <cintya.lara.f@gmail.com>
2025-12-05 00:21:26 -0700

    fix header blank space

--- app/components/empty-header.js
+++ app/components/empty-header.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 export default function EmptyHeader() {
   return (
-    <div className="container">
-      <header className="w-full bg-white py-4 px-6 shadow sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32">
+    <div className="flex">
+      <header className="w-full bg-white py-4 sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32">
         <Image src="/sait-logo.png" alt="Logo" height={36} width={131} />
       </header>
     </div>
